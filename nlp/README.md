27661a08d98f9f2447b1a13a9731e2340b8084e8
SandyHu <sandyhuu@163.com>
2020-07-04 03:03:00 +0800

    server module for nlp

--- nlp/README.md
+++ nlp/README.md
@@ -1,2 +1,20 @@
-# Server for NLP function in HelloSlide
+# NLP for HelloSlide (Server Part)
+## Server Module @sandyhu533
+### use python to serve as a HTTP server. find it all in 'Server.py'.
+### server ip:121.41.224.199; port: 2202
+### receive a file from client :  
+1. Format: No limit
+2. FileName: client ip + "_*_" + origin file name
+3. FileLocation: absolute location / origin /
+### send a file to client : 
+1. Format: Json
+2. FileName: client ip + "_*_" + origin file name + "_*_" + ".json"
+3. FileLocation: absolute location / result /
+### how to use?
+1. set ABSOLUTE_URL to config the absolute location of the resouce repo
+2. implement 
+> def NLP(originFileURL, resultFileURL):
+    logging.debug("here is the nlp process")
+3. set OUTPUT_DEBUG to False to get result
+## NLP Module
 
---
+++ nlp/Server.py
@@ -0,0 +1,63 @@
+from http.server import BaseHTTPRequestHandler
+import io
+import logging
+
+ABSOLUTE_URL = "/root/HelloSlide/"
+OUTPUT_DEBUG = True
+
+class PostHandler(BaseHTTPRequestHandler):
+
+    def do_POST(self):
+
+        # 分析提交的数据(文件长度、文件名称)
+        add = self.address_string()
+        logging.debug("IP:" + add)
+
+        cin = self.rfile
+        message = self.headers
+        fileName = ""
+        if 'File-Name' in message:
+            fileName = message['File-Name']
+        flen = 0
+        if 'Content-Length' in message:
+            flen = int(message['Content-Length'])
+
+        logging.debug("fileName:" + fileName)
+        logging.debug("flen:" + str(flen))
+
+        originFileURL = ABSOLUTE_URL + "origin/" + add + "_*_" + fileName
+        resultFileURL = ABSOLUTE_URL + "result/" + add + "_*_" + fileName + "_*_" + ".json"
+        if OUTPUT_DEBUG:
+            resultFileURL = ABSOLUTE_URL + "result/default.json"
+
+        # 获取传输文件
+        fi = open(originFileURL, "wb+")
+        while flen != 0:
+            tmp = cin.read(min(256, flen))
+            fi.write(tmp)
+            flen -= len(tmp)
+        cin.close()
+        fi.close()
+
+        # 中间处理，调用nlp模块
+        NLP(originFileURL, resultFileURL)
+
+        # 获取结果文件并传输结果
+        self.send_response(200)
+        self.end_headers()
+
+        fo = open(resultFileURL, "rb")
+        cout = self.wfile
+        cout.write(fo.read())
+        fo.close()
+
+def NLP(originFileURL, resultFileURL):
+    logging.debug("here is the nlp process")
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
+    from http.server import HTTPServer
+    server = HTTPServer(('', 2202), PostHandler)
+    logging.debug("------Starting server------")
+    server.serve_forever()
